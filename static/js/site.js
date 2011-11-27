d64554073eab5bdd9a2f83518c79653b69d75beb
Paul Nakata <paul@pmn.org>
2011-11-26 17:13:23 -0700

    More work getting to the MVP

--- static/js/site.js
+++ static/js/site.js
@@ -1,6 +1,9 @@
+notices = [];
+
 function savechanges(itemid, raw_body){
     $.post("/save", { 'entry_id': itemid, 'raw_body': raw_body}, function(data){
 	markpageclean();
+	notify_save_successful(itemid);
     });
 }
 
@@ -11,6 +14,20 @@ function markpagedirty(){
 function markpageclean(){
     document.title = document.title.replace(" *", "");
 }
+
+
+function notify_save_successful(itemid){
+    spanid = "notice_" + itemid.replace(".","");
+    notice = "<span id='" + spanid + "' class='notice'>save complete!</span>";
+    $("#notifications").prepend(notice);
+    
+    notices.push("#"+spanid);
+
+    var x = setTimeout(function(){
+	$("#" + spanid).fadeOut();
+    }, 2000);
+}
+
 
 $(".editbox").focus(function(e){
     markpagedirty();
