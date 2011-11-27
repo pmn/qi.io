a062a297c142dfbc603d091fd6688aa9c86fbc2f
Paul Nakata <paul@pmn.org>
2011-11-26 18:00:35 -0700

    Updated settings for use on heroku

--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
+import os
+
 APP_SECRET_KEY = "1Z|=IYyJu;WFCPHzoYN6^ ErI(*kyoa[<FIY1bV/M4E}WN$9,vcC)tT-:=8,0s8+"
 APP_DEBUG_ENABLED = True
 APP_PORT = 5000
@@ -5,5 +7,5 @@ APP_RELOADER_ENABLED = True
 
 BCRYPT_WORK_FACTOR = 12
 
-MONGODB_HOST = "localhost"
+MONGODB_HOST = os.environ.get('MONGOHQ_URL') or "localhost"
 
