9e35fbc3967a21e36dca978a522e0bd7941c3d59
Paul Nakata <paul@pmn.org>
2011-11-28 18:05:23 -0700

    Messing around with some layout changes

--- static/js/site.js
+++ static/js/site.js
@@ -1,14 +1,7 @@
-notices = [];
-checkactive = false;
-
 function savechanges(itemid, raw_body){
     $.post("/save", { 'entry_id': itemid, 'raw_body': raw_body}, function(data){
 	markpageclean();
-	notify_save_successful(itemid);
-	setTimeout(function(){
-	    spanid = "notice_" + itemid.replace(".","");
-	    $("#" + spanid).fadeOut(300, function() {$("#" + spanid).remove()});
-	}, 2000);
+	notify_save_successful();
     });
 }
 
@@ -20,21 +13,14 @@ function markpageclean(){
     document.title = document.title.replace(" *", "");
 }
 
-function checker(){
-    if (notices.length > 0){
-	console.log("# of notices:" + notices.length);
-	console.log(notices);
-    }
-}
-
-function notify_save_successful(itemid){
-    spanid = "notice_" + itemid.replace(".","");
-    notice = "<span id='" + spanid + "' class='notice'>save complete!</span>";
-    $("#notifications").prepend(notice);
-
+function notify_save_successful(){
+    $("#savecomplete").show();
+    
+    setTimeout(function(){
+	$("#savecomplete").fadeOut(300);
+    }, 2000);
 
 }
-
 
 $(".editbox").focus(function(e){
     markpagedirty();
